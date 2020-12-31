a68692d006d3d4ad192be8b38b9f9a4af88fdbae
Austin Hernandez <ahernandez1170@gmail.com>
2020-12-31 03:14:06 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,12 @@
 def unsafe?(speed)
-
-end
-
-
+if speed < 40 || speed > 60
+  return unsafe
+else 
+  return false
+  end
 
 def not_safe?(speed)
-	
+ if speed < 40 || speed > 60 ? unsafe : false
 end
 	
 
